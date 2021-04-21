fc5d35ef168d54bdd896ece7694f6870599685d8
xdFancy <79912054+xdFancy@users.noreply.github.com>
2021-04-21 19:07:30 +0300

    Doors

---
+++ Assets/Scripts/DoorPresurePlate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorPresurePlate : MonoBehaviour
+{
+    [SerializeField] private Door door;
+
+    private float timer;
+
+    private void Update()
+    {
+        if (timer > 0f)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                door.CloseDoor();
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        float timeToStayOpen = 2f;
+        timer = timeToStayOpen;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        door.OpenDoor();
+    }
+
+}
